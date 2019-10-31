942803d005ce9ddcf217853bead9217138a4d1b4
Cheryl <liaochery@yahoo.com>
2019-10-31 22:19:55 +0800

    usecontext for user email in SignedInMenu

--- src/features/nav/Menus/SignedInMenu.jsx
+++ src/features/nav/Menus/SignedInMenu.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { Menu,Image, Dropdown} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 import { auth } from '../../auth/firebase.utils'
 import { connect } from 'react-redux';
+import CurrentUserContext from '../../../app/contexts/current-user/current-user.context';
 // import {signOut} from '../../auth/authActions'
 //import signOut again.
-const SignedInMenu = ({currentUser}) => {
+const SignedInMenu = () => {
+  const currentUser = useContext(CurrentUserContext)
   return (
         <Menu.Item position="right">
           <Image avatar spaced="right" src='/assets/user.png' />
-          <Dropdown pointing="top left" text= {currentUser}>
+          <Dropdown pointing="top left" text= {currentUser.email}>
             <Dropdown.Menu>
               <Dropdown.Item text="Create Event" icon="plus" />
               <Dropdown.Item text="My Events" icon="calendar" />
